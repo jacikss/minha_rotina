bd2dd8bc889dc894b593c822a4526b9458c1bc6f
jacikss <103142042+jacikss@users.noreply.github.com>
2024-12-09 21:20:37 +0000

    inicial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Minharotina } from './components/Minharotina'
+
+function App() {
+
+  return (
+    <>
+      <Minharotina/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Minharotina.jsx
@@ -0,0 +1,51 @@
+import "./Minharotina.css"
+export function Minharotina() {
+    return <div>
+        <h1>Minha Rotina (Dias da Semana)</h1>
+        <section>
+            <h2>
+                Manhã
+            </h2>
+            <ul>
+                <li>Estágio (8hs às 12hs) </li>
+            </ul>
+        </section>
+        <section>
+            <h2>
+                Tarde
+            </h2>
+            <ul>
+                <li>Almoço</li>
+                <li>Trabalhar no comércio dos meus pais (14hs às 17:30hs)</li>
+            </ul>
+        </section>
+        <section>
+            <h2>
+                Noite
+            </h2>
+            <ul>
+                <li>Faculdade (18:30hs às 22hs) </li>
+                <li>Dormir</li>
+            </ul>
+        </section>
+        <h1>Minha Rotina (Finais de Semana)</h1>
+        <section>
+            <h2>                
+                Sábado
+            </h2>
+            <ul>
+                <li>Trabalhar no comércio dos meus pais (9hs às 17:30hs) </li>
+            </ul>
+        </section>
+        <section>
+            <h2>
+                Domingo
+            </h2>
+            <ul>
+                <li>Trabalhar no comércio dos meus pais (9hs às 13hs) </li>
+                <li>Descansar</li>
+                <li>Igreja (19:30hs às 21hs) </li>
+            </ul>
+        </section>
+    </div>
+}
